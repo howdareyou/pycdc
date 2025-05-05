4e1e945765a46fa444b47b412445a38013ba98aa
howdareyou <feng.xiaoyu.fxy@outlook.com>
2025-05-05 16:47:15 +0800

    - Chore: Formatted some files. - Fixed: Incorrectly parsing keyword arguments - Feat: Short-circuit evaluation support

--- ASTree.h
+++ ASTree.h
@@ -2,10 +2,11 @@
 #define _PYC_ASTREE_H
 
 #include "ASTNode.h"
+#include "pyc_module.h"
 
-PycRef<ASTNode> BuildFromCode(PycRef<PycCode> code, PycModule* mod);
-void print_src(PycRef<ASTNode> node, PycModule* mod, std::ostream& pyc_output);
+PycRef<ASTNode> BuildFromCode(PycRef<PycCode> code, PycModule *mod);
+void print_src(PycRef<ASTNode> node, PycModule *mod, std::ostream &pyc_output);
 
-void decompyle(PycRef<PycCode> code, PycModule* mod, std::ostream& pyc_output);
+void decompyle(PycRef<PycCode> code, PycModule *mod, std::ostream &pyc_output);
 
 #endif
--- pyc_string.h
+++ pyc_string.h
@@ -1,37 +1,35 @@
 #ifndef _PYC_STRING_H
 #define _PYC_STRING_H
 
-#include "pyc_object.h"
 #include "data.h"
-#include <cstdio>
+#include "pyc_object.h"
 #include <string>
+
 
 class PycString : public PycObject {
 public:
-    PycString(int type = TYPE_STRING)
-        : PycObject(type) { }
+  PycString(int type = TYPE_STRING) : PycObject(type) {}
 
-    bool isEqual(PycRef<PycObject> obj) const override;
-    bool isEqual(const std::string& str) const { return m_value == str; }
+  bool isEqual(PycRef<PycObject> obj) const override;
+  bool isEqual(const std::string &str) const { return m_value == str; }
 
-    bool startsWith(const std::string& str) const
-    {
-        return m_value.substr(0, str.size()) == str;
-    }
+  bool startsWith(const std::string &str) const {
+    return m_value.substr(0, str.size()) == str;
+  }
 
-    void load(class PycData* stream, class PycModule* mod) override;
+  void load(class PycData *stream, class PycModule *mod) override;
 
-    int length() const { return (int)m_value.size(); }
-    const char* value() const { return m_value.c_str(); }
-    const std::string &strValue() const { return m_value; }
+  int length() const { return (int)m_value.size(); }
+  const char *value() const { return m_value.c_str(); }
+  const std::string &strValue() const { return m_value; }
 
-    void setValue(std::string str) { m_value = std::move(str); }
+  void setValue(std::string str) { m_value = std::move(str); }
 
-    void print(std::ostream& stream, class PycModule* mod, bool triple = false,
-               const char* parent_f_string_quote = nullptr);
+  void print(std::ostream &stream, class PycModule *mod, bool triple = false,
+             const char *parent_f_string_quote = nullptr);
 
 private:
-    std::string m_value;
+  std::string m_value;
 };
 
 #endif
